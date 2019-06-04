b13041374a9312913743450198f948f1ca82858f
Xie <xcgoner1108@gmail.com>
2019-06-04 16:20:59 -0700

    automatically sync scripts

--- create_cluster.sh
+++ create_cluster.sh
@@ -12,15 +12,15 @@ done
 
 # hudl -t -v -h $HOSTS -d ~ -s dependency.sh eric-haibin-lin raw mxnet-cu90==1.5.0b20190525 mx-0525-raw-large-cased 2177746 90 py27;
 # hudl -t -v -h $HOSTS -d /home/ubuntu -s dependency.sh dmlc raw mxnet-cu100 bert-baseline 2177746 100 py;
-hudl -t -v -h $HOSTS -d ~ -s dependency.sh dmlc raw mxnet-cu90==1.5.0b20190525 bert-baseline 2177746 90 py27;
+hudl -t -v -h $HOSTS -d ~ -s dependency.sh dmlc master mxnet-cu90==1.5.0b20190525 xcong-bert-baseline 2177746 90 py27;
 
 hudl -h $HOSTS -v 'cat .status';
 
 export MASTER=$(head -n 1 $HOSTS)
 
-ssh $MASTER ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+# ssh $MASTER ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
-export PUBKEY=$(ssh $MASTER 'cat .ssh/id_rsa.pub');
-hudl -h $HOSTS "echo $PUBKEY >> ~/.ssh/authorized_keys";
+# export PUBKEY=$(ssh $MASTER 'cat .ssh/id_rsa.pub');
+# hudl -h $HOSTS "echo $PUBKEY >> ~/.ssh/authorized_keys";
 
-hudl -h $HOSTS -v 'cat ~/.ssh/authorized_keys';
+# hudl -h $HOSTS -v 'cat ~/.ssh/authorized_keys';
